2657b86ad099b6aa43bcbaa17f8b6935a0ba79b5
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-26 15:54:27 -0600

    added actual descriptions to highways

--- app/controllers/i40Controller.js
+++ app/controllers/i40Controller.js
@@ -1,5 +1,5 @@
 "use strict";
 angular.module("Routes").controller("i40Ctrl", function ($scope){
   $scope.highwayName = "Interstate 40";
-  $scope.highwayDescription = "This is a description of Interstate 40";
+  $scope.highwayDescription = "Interstate 40 (I-40) is a major east-west Interstate Highway running through the south-central portion of the United States generally north of Interstate 10 and Interstate 20 but south of Interstate 70. The western end is at Interstate 15 in Barstow, California; its eastern end is at a concurrency of U.S. Route 117 and North Carolina Highway 132 in Wilmington, North Carolina. It is the third longest interstate in the United States, behind Interstate 80 and Interstate 90. Much of the western part of I-40, from Oklahoma City to Barstow, parallels or overlays the historic U.S. Route 66, east of Oklahoma City the route generally parallels U.S. Route 64 and U.S. Route 70. I-40 runs through many major cities including Albuquerque, New Mexico; Amarillo, Texas; Oklahoma City, Oklahoma; Little Rock, Arkansas; Memphis, Tennessee; Nashville, Tennessee; Knoxville, Tennessee; and Raleigh, North Carolina.";
 });
